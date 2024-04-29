17590657e987c015d54f42acc47850368a7b5891
kacan98 <kacan@dynaway.com>
2024-04-29 23:54:53 +0200

    Make font sizes responsive

--- app/theme.ts
+++ app/theme.ts
@@ -1,56 +1,58 @@
 "use client";
-import { createTheme } from "@mui/material/styles";
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 
-const theme = createTheme({
-  palette: {
-    mode: "dark",
-    primary: {
-      main: "#1976d2",
+const theme = responsiveFontSizes(
+  createTheme({
+    palette: {
+      mode: "dark",
+      primary: {
+        main: "#1976d2",
+      },
+      info: {
+        main: "#fff",
+      },
+      action: {
+        active: "#fff", // choose a suitable color for icons
+      },
     },
-    info: {
-      main: "#fff",
-    },
-    action: {
-      active: "#fff", // choose a suitable color for icons
-    },
-  },
-  typography: {
-    fontFamily: "system-ui, sans-serif",
-    allVariants: {
-      color: "#fff",
-    },
+    typography: {
+      fontFamily: "system-ui, sans-serif",
+      allVariants: {
+        color: "#fff",
+      },
 
-    h1: {
-      fontWeight: 700,
+      h1: {
+        fontWeight: 700,
+      },
+      h2: {
+        fontWeight: 700,
+      },
+      h3: {
+        fontWeight: 700,
+      },
+      h4: {
+        fontWeight: 700,
+      },
+      h5: {
+        fontWeight: 700,
+      },
+      h6: {
+        fontWeight: 700,
+      },
+      button: {
+        fontWeight: 500,
+      },
     },
-    h2: {
-      fontWeight: 700,
-    },
-    h3: {
-      fontWeight: 700,
-    },
-    h4: {
-      fontWeight: 700,
-    },
-    h5: {
-      fontWeight: 700,
-    },
-    h6: {
-      fontWeight: 700,
-    },
-    button: {
-      fontWeight: 500,
-    },
-  },
-  components: {
-    MuiIcon: {
-      styleOverrides: {
-        root: {
-          color: "#fff",
+    components: {
+      MuiIcon: {
+        styleOverrides: {
+          root: {
+            color: "#fff",
+          },
         },
       },
     },
-  },
-});
+  }),
+);
 
 export default theme;
